d9e46d544cfe76c1ed170788a659b7cc07eda873
Andrew Barna <abarna@gmail.com>
2016-07-05 16:09:15 -0700

    return jwt on login

--- cchdoapi/__init__.py
+++ cchdoapi/__init__.py
@@ -9,14 +9,37 @@ app.config["SECRET_KEY"] = "SOME SECRET"
 db = SQLAlchemy(app)
 
 from flask import jsonify, request
+from werkzeug.exceptions import Unauthorized
 
 import jwt
 
 from .models import User
 
-@app.route("/testdb")
-def test_db():
-    return "ok"
+
+# Temporary workaround until #941 is fixed
+# https://github.com/pallets/flask/issues/941
+exceptions = (Unauthorized, )
+
+def handle_http_exception(error):
+	return jsonify({
+            'status_code': error.code,
+            'message': str(error),
+            'description': error.description
+        }), error.code
+
+for exception in exceptions:
+    app.errorhandler(exception)(handle_http_exception)
+
+@app.route("/test_jwt")
+def test_jwt():
+    auth = request.headers.get("Authorization")
+    if auth and auth.startswith("Barrer "):
+        token = auth.split("Barrer ")[1].strip()
+        try:
+            token = jwt.decode(token, app.config["SECRET_KEY"])
+        except:
+            raise Unauthorized()
+        return str(token)
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -24,12 +47,23 @@ def register():
 
 @app.route("/login", methods=["POST"])
 def index():
-    print(request.get_json())
-    token = {"sub":1,}
-    token = jwt.encode(token, app.config["SECRET_KEY"], algorithm='HS256')
-    token = token.decode('utf-8')
+    credentials = request.get_json()
+    email = credentials.get("email")
+    password = credentials.get("password")
+    if email is not None:
+        try:
+            user = User.query.filter_by(email=email).one()
+        except Exception as e:
+            raise Unauthorized()
 
-    return jsonify(access_token=token)
+    if user.verify(password):
+        token = {"sub":user.id,}
+        token = jwt.encode(token, app.config["SECRET_KEY"], algorithm='HS256')
+        token = token.decode('utf-8')
+
+        return jsonify(access_token=token)
+
+    raise Unauthorized()
 
 @app.cli.command()
 @click.option("--clean", is_flag=True)
