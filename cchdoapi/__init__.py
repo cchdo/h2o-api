756cdf820ee333f463bb4d0f00d050c1489333d7
Andrew Barna <abarna@gmail.com>
2016-09-15 12:52:50 -0700

    echo sqlalchmy only in debug mode

--- cchdoapi/__init__.py
+++ cchdoapi/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/cchdoapi"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_ECHO"] = app.config["DEBUG"]
 app.config["SECRET_KEY"] = "SOME SECRET"
 db = SQLAlchemy(app)
 
